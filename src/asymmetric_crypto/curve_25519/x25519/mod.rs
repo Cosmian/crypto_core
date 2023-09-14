d9b378d13480f43c51004f5cc0afcc5a7b7103a6
Manuthor <manu.coste@gmail.com>
2023-09-14 08:56:55 +0200

    chore: squash commits and rebase on develop

--- src/asymmetric_crypto/curve_25519/x25519/mod.rs
+++ src/asymmetric_crypto/curve_25519/x25519/mod.rs
@@ -7,7 +7,4 @@ mod public_key;
 pub use private_key::X25519PrivateKey;
 pub use public_key::{X25519PublicKey, X25519_PUBLIC_KEY_LENGTH};
 
-use super::private_key::Curve25519PrivateKey;
-
-pub type X25519PrivateKey = Curve25519PrivateKey;
 pub use key_pair::X25519Keypair;
