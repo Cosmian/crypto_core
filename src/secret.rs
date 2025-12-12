369d10346f9e27abd99e748f61977ccdda7a6f68
Théophile BRÉZOT <theophile.brezot@cosmian.com>
2025-12-12 18:03:45 +0100

    add a method to derive a new secret from an existing one

--- src/secret.rs
+++ src/secret.rs
@@ -52,6 +52,41 @@ impl<const LENGTH: usize> Secret<LENGTH> {
         bytes.zeroize();
         secret
     }
+
+    /// Deterministically derive a new secret from the given secret and
+    /// additional information.
+    ///
+    /// # Error
+    ///
+    /// Fails to generate the new secret in case the source evidently does not
+    /// contain enough entropy. The check performed is based on the respective
+    /// new- and source-secret lengths. The source secret needs to be have a
+    /// length greater than the desired new-secret length for this check to
+    /// pass.
+    #[cfg(feature = "sha3")]
+    pub fn derive<const SECRET_LENGTH: usize>(
+        source: &Secret<SECRET_LENGTH>,
+        info: &[u8],
+    ) -> Result<Self, CryptoCoreError> {
+        use crate::kdf256;
+
+        if info.is_empty() {
+            return Err(CryptoCoreError::InvalidBytesLength(
+                "info is a required field as it is used as domain separation".to_string(),
+                0,
+                None,
+            ));
+        }
+        if SECRET_LENGTH < LENGTH {
+            return Err(CryptoCoreError::ConversionError(format!(
+                "insufficient entropy to derive {}-byte secret from a {}-byte secret",
+                LENGTH, SECRET_LENGTH,
+            )));
+        }
+        let mut target = Self::default();
+        kdf256!(&mut *target, &**source, info);
+        Ok(target)
+    }
 }
 
 impl<const LENGTH: usize> Default for Secret<LENGTH> {
